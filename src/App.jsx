1d88b16dbb33e9c58f4627bd63f04b3fceba32cf
mehdi-Har <mehdiharim6.com>
2024-02-02 21:12:04 +0100

    tracker expenses

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
-import ToDoList from "./toDolist"
+import { Header } from "./components/Header"
+import { Balance } from "./components/Balance";
+import { IncomeExpenses } from "./components/IncomeExpenses";
+import { TransactionList } from "./components/TransactionList";
+import { AddTransaction } from "./components/AddTransaction";
+import { GlobalProvider } from "./context/GlobalState";
 function App() {
-  return(<>
-          <ToDoList></ToDoList>
-       </>)
+  return(
+  <GlobalProvider>
+    <Header/>
+    <div className="container">
+      <Balance/>
+      <IncomeExpenses/>
+      <TransactionList/>
+      <AddTransaction/>
+    </div>
+  </GlobalProvider>
+  );
 }
 
 export default App
